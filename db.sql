b5bba40dc90300726304a24a958f71705e80be37
Diego Perozo <diegodapt18@gmail.com>
2022-04-08 19:00:00 -0400

    adding the project files to the repo

---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE students-example;
+CREATE TABLE students(id Serial, name text, address text, age int);
+
+INSERT INTO students(name, address, age) VALUES ('joe', 'LA', 30);
+INSERT INTO students(name, address, age) VALUES ('ryan', 'San francisco', 23);
